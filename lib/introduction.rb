a96bc4c622c883f3870132547605ba369d620153
Jason Easterly <jason.easterly11@gmail.com>
2020-08-16 17:38:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,10 +13,9 @@ def introduction_with_language name, language
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end    
 
-introduction_with_language_optional 
-
-
-
+def introduction_with_language+optional name, language = "Ruby"
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end   
 
 
 
